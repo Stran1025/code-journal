dbe1ee8b22bbb7ceb01f99630ec5c571ff472534
Sang Tran <sang.tran1025@gmail.com>
2022-04-04 19:53:19 +0000

    Issue #1 - added photo updates based on url provided.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+
+var $photoDisplay = document.querySelector('#photo-display');
+var $urlInput = document.querySelector('#url-input');
+
+$urlInput.addEventListener('focusout', updatePhoto);
+
+function updatePhoto(event) {
+  $photoDisplay.setAttribute('src', event.target.value);
+}
