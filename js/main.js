232899a6d91080264e2fec71011aff3427fd83a6
Sang Tran <sang.tran1025@gmail.com>
2022-04-04 21:42:00 +0000

    added all the input data into an new object

--- js/main.js
+++ js/main.js
@@ -2,10 +2,23 @@
 /* exported data */
 
 var $photoDisplay = document.querySelector('#photo-display');
-var $urlInput = document.querySelector('#url-input');
+var $urlInput = document.querySelector('#url');
+var $form = document.querySelector('#input-form');
 
 $urlInput.addEventListener('focusout', updatePhoto);
+$form.addEventListener('submit', saveEntry);
+
+function saveEntry(event) {
+  event.preventDefault();
+  var obj = {};
+  obj.title = $form.elements.title.value;
+  obj.photoURL = $form.elements.url.value;
+  obj.notes = $form.elements.note.value;
+}
 
 function updatePhoto(event) {
+  if (event.target.value === '') {
+    return;
+  }
   $photoDisplay.setAttribute('src', event.target.value);
 }
