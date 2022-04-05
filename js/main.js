43456c7544d50f5d832bf04cd1a8e28f74bf21ee
Sang Tran <sang.tran1025@gmail.com>
2022-04-05 06:45:13 +0000

    Issue #2 - New entries get added to the entries tab, new entry get added without reloading.

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ var $entries = document.querySelector('ul.container');
 var $tabsContainer = document.querySelector('#tabs');
 var $tab = document.querySelectorAll('.tab');
 var $view = document.querySelectorAll('.view');
+// var $newEntryButton = document.querySelector('#new-entry-button');
 
 $urlInput.addEventListener('input', updatePhoto);
 $form.addEventListener('submit', saveEntry);
-document.addEventListener('DOMContentLoaded', displayingEntry);
+document.addEventListener('DOMContentLoaded', displayingPreviousEntry);
 $tabsContainer.addEventListener('click', switchTab);
+// $newEntryButton.addEventListener('click', switchToForm);
 
 function saveEntry(event) {
   event.preventDefault();
@@ -24,6 +26,7 @@ function saveEntry(event) {
   data.nextEntryId++;
   data.entries.unshift(obj);
   $form.reset();
+  displayNewEntry(obj);
   $photoDisplay.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
 
@@ -69,11 +72,20 @@ function createLi(obj) {
   return $li;
 }
 
-function displayingEntry(event) {
+function displayNewEntry(obj) {
+  var liElement = createLi(obj);
+  $entries.appendChild(liElement);
+}
+
+function displayingPreviousEntry(event) {
   if (!data.entries.length) {
     var $noEntry = document.createElement('p');
+    var $noEntryDiv = document.createElement('div');
+    $noEntryDiv.className = 'width-full full-page padding';
     $noEntry.textContent = 'No entries have been recorded.';
-    $entries.appendChild($noEntry);
+    $noEntry.classList = 'width-full';
+    $noEntryDiv.appendChild($noEntry);
+    $entries.appendChild($noEntryDiv);
   }
   for (var i = 0; i < data.nextEntryId - 1; i++) {
     var liElement = createLi(data.entries[i]);
@@ -85,18 +97,23 @@ function switchTab(event) {
   if (!event.target.matches('.tab')) {
     return;
   }
-  for (var i = 0; i < $tab.length; i++) {
-    if ($tab[i] === event.target) {
+  for (var tabIndex = 0; tabIndex < $tab.length; tabIndex++) {
+    if ($tab[tabIndex] === event.target) {
       event.target.classList.add('active');
     } else {
-      $tab[i].classList.remove('active');
+      $tab[tabIndex].classList.remove('active');
     }
   }
-  for (var j = 0; j < $view.length; j++) {
-    if ($view[j].getAttribute('data-view') === event.target.getAttribute('data-view')) {
-      $view[j].classList.remove('hidden');
+  for (var viewIndex = 0; viewIndex < $view.length; viewIndex++) {
+    if ($view[viewIndex].getAttribute('data-view') === event.target.getAttribute('data-view')) {
+      $view[viewIndex].classList.remove('hidden');
     } else {
-      $view[j].classList.add('hidden');
+      $view[viewIndex].classList.add('hidden');
     }
   }
 }
+
+// function switchToForm(event) {
+//   for (var tabIndex = 0; tabIndex < $tab; tabIndex++) {
+//   }
+// }
